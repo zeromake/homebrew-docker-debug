e162cd6a4a24e20d6a9dc381e1287c15bf8720ad
zeromake <a390720046@gmail.com>
2019-03-28 14:11:49 +0800

    feat: add brew pkg desc

--- docker-debug.rb
+++ docker-debug.rb
@@ -4,6 +4,7 @@ REPOSITORY_URL='https://github.com/zeromake/docker-debug'
 HOMEBREW_GIT_VERSION='v0.2.2'
 
 class DockerDebug < Formula
+  desc "debug docker container"
   homepage "#{REPOSITORY_URL}"
   url "#{REPOSITORY_URL}/releases/download/#{HOMEBREW_GIT_VERSION}/docker-debug-darwin-amd64"
   sha256 "bc2d39c0fab3142c01ee5011618b8b7c63a5f7298d9be8a14a06cfb543189887"
