364a43bece3694458cb71911f9e38c2ec3a59164
zeromake <a390720046@gmail.com>
2020-05-08 17:27:29 +0800

    feat: update version to v0.7.3

--- docker-debug.rb
+++ docker-debug.rb
@@ -1,13 +1,13 @@
 require "formula"
 
 REPOSITORY_URL='https://github.com/zeromake/docker-debug'
-HOMEBREW_GIT_VERSION='0.7.2'
+HOMEBREW_GIT_VERSION='0.7.3'
 
 class DockerDebug < Formula
   desc "debug docker container"
   homepage "#{REPOSITORY_URL}"
   url "#{REPOSITORY_URL}/releases/download/#{HOMEBREW_GIT_VERSION}/docker-debug-darwin-amd64"
-  sha256 "913f6dd393c47ef35eff226412b150db31a85ca50f16a13e60ab2e90d4a916c0"
+  sha256 "28d3aaf47f16f5e9aa87c231cb373b74a3486c4c0d1eb4352ceeb5cb8f0b4b29"
   head "#{REPOSITORY_URL}.git"
   version "#{HOMEBREW_GIT_VERSION}"
 
