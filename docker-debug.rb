e8c573371b99c681a87bc51eba69635b42b54a6d
zeromake <a390720046@gmail.com>
2024-08-11 16:33:28 +0800

    Update docker-debug.rb

--- docker-debug.rb
+++ docker-debug.rb
@@ -1,7 +1,7 @@
 require "formula"
 
 REPOSITORY_URL='https://github.com/zeromake/docker-debug'
-HOMEBREW_GIT_VERSION='0.7.7'
+HOMEBREW_GIT_VERSION='0.7.8'
 
 class DockerDebug < Formula
   arch = Hardware::CPU.intel? ? "amd64" : "arm64"
@@ -9,9 +9,9 @@ class DockerDebug < Formula
   homepage "#{REPOSITORY_URL}"
   url "#{REPOSITORY_URL}/releases/download/v#{HOMEBREW_GIT_VERSION}/docker-debug-darwin-#{arch}"
   if Hardware::CPU.intel?
-    sha256 "728ecd9237d4ae7163dc5897b43cd6e835ce7bf8e8bde55c148a276689ee2050"
+    sha256 "398b7df224a1b3a4dc85422bc80b25b2bd0386b2102e058f66f5a351b2352aa0"
   else
-    sha256 "8539ce818073ad33c4b248e3bafac07077794d45d56caf11f29a54c79de5c3d5"
+    sha256 "f6332956ff72d19559aed5cf26d22851fe78f8725d036f7fd2da40be37e4b824"
   end
   head "#{REPOSITORY_URL}.git"
   version "#{HOMEBREW_GIT_VERSION}"
